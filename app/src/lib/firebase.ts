bf39155aa714e8e84143f52eb1fa7e5770e0eb0f
Mike Thomas <mikethomas84@gmail.com>
2023-08-08 16:46:32 +0100

    fb config

---
+++ app/src/lib/firebase.ts
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBS9BWqIlsmciVflUS3bVgV944boe6bURo",
+  authDomain: "svelt-app.firebaseapp.com",
+  projectId: "svelt-app",
+  storageBucket: "svelt-app.appspot.com",
+  messagingSenderId: "230001371803",
+  appId: "1:230001371803:web:a0b548574f4e28e5efb43e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export const auth = getAuth();
+export const storage = getStorage();
