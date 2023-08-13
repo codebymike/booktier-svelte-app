58dde7a30447cd98fe06aef190e15533e4830790
Mike Thomas <mikethomas84@gmail.com>
2023-08-13 23:53:16 +0100

    exists db check

--- app/src/routes/login/username/+page.svelte
+++ app/src/routes/login/username/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import AuthCheck from "$lib/components/AuthCheck.svelte";
+    import { db, user } from "$lib/firebase";
+    import { doc, getDoc, writeBatch} from "firebase/firestore";
 
     let username = "";
     let loading = false;
@@ -13,7 +15,14 @@
 
         loading = true;
 
-        debounceTimer = setTimeout(async () => {
+        debounceTimer = setTimeout( async () => {
+            console.log("checking availability of", username);
+        
+            const ref = doc(db, "usernames", username);
+            const exists = await getDoc(ref).then((doc) => doc.exists());
+
+            isAvailable = !exists;
+            loading = false;
         }, 500);
     }
 
