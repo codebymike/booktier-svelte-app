84027455bd6cc31b3a4661ddfd829dd9b6f464a2
Mike Thomas <mikethomas84@gmail.com>
2023-08-13 23:50:08 +0100

    form + local vars

--- app/src/routes/login/username/+page.svelte
+++ app/src/routes/login/username/+page.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
     import AuthCheck from "$lib/components/AuthCheck.svelte";
+
+    let username = "";
+    let loading = false;
+    let isAvailable = false;
+
+    async function confirmUsername() {
+    // TODO
+    }
+
 </script>
 
 
 
 <AuthCheck>
     <h2>Username</h2>
+    <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
+
+
+      </form>
 </AuthCheck>
