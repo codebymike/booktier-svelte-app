4e8b1e1e4ce098e57919b2ad4c3217fd57360f59
Mike Thomas <mikethomas84@gmail.com>
2023-08-14 14:04:55 +0100

    reactive bools

--- app/src/routes/login/username/+page.svelte
+++ app/src/routes/login/username/+page.svelte
@@ -11,7 +11,8 @@
 
     const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
     $: isValid = username?.length > 2 && username.length < 16 && re.test(username);
-
+    $: isTouched = username.length > 0;
+    $: isTaken = isValid && !isAvailable && !loading
 
     async function checkAvailability() {
         isAvailable = false;
