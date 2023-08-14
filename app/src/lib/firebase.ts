742032184c5fabd54c05e9ab08bcd2cd6c1e7ccd
Mike Thomas <mikethomas84@gmail.com>
2023-08-14 14:17:49 +0100

    docstore generic

--- app/src/lib/firebase.ts
+++ app/src/lib/firebase.ts
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { doc, getFirestore, onSnapshot } from "firebase/firestore";
 import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { writable } from "svelte/store";
@@ -49,3 +49,30 @@ function userStore() {
 }
 
 export const user = userStore();
+
+
+/**
+ * @param  {string} path document path or reference
+ * @returns a store with realtime updates on document data
+ */
+export function docStore<T>(
+  path: string,
+) {
+  let unsubscribe: () => void;
+
+  const docRef = doc(db, path);
+
+  const { subscribe } = writable<T | null>(null, (set) => {
+    unsubscribe = onSnapshot(docRef, (snapshot) => {
+      set((snapshot.data() as T) ?? null);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+    ref: docRef,
+    id: docRef.id,
+  };
+}
