8de295b8966e838674f05d49b584be58e612c0d7
Mike Thomas <mikethomas84@gmail.com>
2023-08-15 11:43:20 +0100

    preview url + userData

--- app/src/routes/login/photo/+page.svelte
+++ app/src/routes/login/photo/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
 
+    import { user, userData, storage, db } from "$lib/firebase";
+
+    let previewURL: string;
+    let uploading = false;
+    $: href = `/${$userData?.username}/edit`;
+
 </script>
 
 <h2>Photo</h2>
