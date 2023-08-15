7fd9af49d49b42cf18a2754fc0219afb6e0ab635
Mike Thomas <mikethomas84@gmail.com>
2023-08-15 11:43:55 +0100

    upload func stub

--- app/src/routes/login/photo/+page.svelte
+++ app/src/routes/login/photo/+page.svelte
@@ -6,6 +6,11 @@
     let uploading = false;
     $: href = `/${$userData?.username}/edit`;
 
+    async function upload(e: any) {
+        uploading = true;
+        const file = e.target.files[0];
+    }
+
 </script>
 
 <h2>Photo</h2>
