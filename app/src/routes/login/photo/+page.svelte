220454216999fc6d8b8a64ea6f2428edee1250dc
Mike Thomas <mikethomas84@gmail.com>
2023-08-15 11:47:05 +0100

    photo form labels

--- app/src/routes/login/photo/+page.svelte
+++ app/src/routes/login/photo/+page.svelte
@@ -34,6 +34,20 @@
               height="256"
               class="mx-auto"
             />
+            <label for="photoURL" class="label">
+              <span class="label-text">Pick a file</span>
+            </label>
+            <input
+              on:change={upload}
+              name="photoURL"
+              type="file"
+              class="file-input file-input-bordered w-full max-w-xs"
+              accept="image/png, image/jpeg, image/gif, image/webp"
+            />
+            {#if uploading}
+              <p>Uploading...</p>
+              <progress class="progress progress-info w-56 mt-6" />
+            {/if}
           </div>
     </form>
 
