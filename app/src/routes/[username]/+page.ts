855e9fabdc3d9900f14482a440f794151442bff0
Mike Thomas <mikethomas84@gmail.com>
2023-08-21 13:43:06 +0100

    query

--- app/src/routes/[username]/+page.ts
+++ app/src/routes/[username]/+page.ts
@@ -1,10 +1,16 @@
 import { db } from '$lib/firebase';
-import { collection } from 'firebase/firestore';
+import { collection, limit, query, where } from 'firebase/firestore';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ params }) => {
     
     const collectionRef = collection(db, "users");
+
+    const q = query(
+        collectionRef,
+        where("username", "==", params.username),
+        limit(1)
+    );
 
     return {};
 }) satisfies PageLoad;
