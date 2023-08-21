001d73014f2f9ed29faba812abe17718413f77f7
Mike Thomas <mikethomas84@gmail.com>
2023-08-21 13:43:43 +0100

    get snapshot

--- app/src/routes/[username]/+page.ts
+++ app/src/routes/[username]/+page.ts
@@ -1,5 +1,5 @@
 import { db } from '$lib/firebase';
-import { collection, limit, query, where } from 'firebase/firestore';
+import { collection, getDocs, limit, query, where } from 'firebase/firestore';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ params }) => {
@@ -11,6 +11,10 @@ export const load = (async ({ params }) => {
         where("username", "==", params.username),
         limit(1)
     );
+
+    const snapshot = await getDocs(q);
+    const exists = snapshot.docs[0]?.exists();
+    const data = snapshot.docs[0]?.data();
 
     return {};
 }) satisfies PageLoad;
