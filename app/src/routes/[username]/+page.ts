2ab371dfef7c23855457c57c1e73def459f57add
Mike Thomas <mikethomas84@gmail.com>
2023-08-21 13:42:43 +0100

    collection

--- app/src/routes/[username]/+page.ts
+++ app/src/routes/[username]/+page.ts
@@ -1,5 +1,10 @@
+import { db } from '$lib/firebase';
+import { collection } from 'firebase/firestore';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ params }) => {
+    
+    const collectionRef = collection(db, "users");
+
     return {};
 }) satisfies PageLoad;
