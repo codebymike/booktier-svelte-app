c36c0bb4e9724871921279db7ad5d4f96b6a48b0
Mike Thomas <mikethomas84@gmail.com>
2023-08-21 13:44:33 +0100

    exists, notpublished guards

--- app/src/routes/[username]/+page.ts
+++ app/src/routes/[username]/+page.ts
@@ -1,6 +1,7 @@
 import { db } from '$lib/firebase';
 import { collection, getDocs, limit, query, where } from 'firebase/firestore';
 import type { PageLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
 export const load = (async ({ params }) => {
     
@@ -15,6 +16,14 @@ export const load = (async ({ params }) => {
     const snapshot = await getDocs(q);
     const exists = snapshot.docs[0]?.exists();
     const data = snapshot.docs[0]?.data();
+
+    if (!exists) {
+        throw error(404, "that user does not exist!");
+    }
+
+    if (!data.published) {
+        throw error(403, `The profile of @${data.username} is not public!`);
+    }
 
     return {};
 }) satisfies PageLoad;
