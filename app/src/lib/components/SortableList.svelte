8804be20404fcf3aad00203cf00ce3d87c8834c5
Mike Thomas <mikethomas84@gmail.com>
2023-08-23 15:43:08 +0100

    vars, createventdisp

--- app/src/lib/components/SortableList.svelte
+++ app/src/lib/components/SortableList.svelte
@@ -1,4 +1,10 @@
 <script lang="ts">
     import { flip } from "svelte/animate";
     import { createEventDispatcher } from "svelte";
+
+    export let list: any[];
+    let isOver: string | boolean = false;
+    const dispatch = createEventDispatcher();
+
+    
 </script>
