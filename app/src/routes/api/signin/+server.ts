3bac34df57672133425e218c82e23879218c1d7a
Mike Thomas <mikethomas84@gmail.com>
2023-08-24 15:35:11 +0100

    req cookie param

--- app/src/routes/api/signin/+server.ts
+++ app/src/routes/api/signin/+server.ts
@@ -1,5 +1,5 @@
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async () => {
+export const POST: RequestHandler = async ({ request, cookies }) => {
     return new Response();
 };
