343faf523107033ffed2ba2e97ba412d13392c7a
Mike Thomas <mikethomas84@gmail.com>
2023-08-24 15:46:45 +0100

    using signin route

--- app/src/routes/login/+page.svelte
+++ app/src/routes/login/+page.svelte
@@ -3,9 +3,19 @@
     import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
     async function signInWithGoogle() {
-        const provider = new GoogleAuthProvider();
-        const user = await signInWithPopup(auth, provider);
-        console.log(user)
+      const provider = new GoogleAuthProvider();
+      const credential = await signInWithPopup(auth, provider);
+
+      const idToken = await credential.user.getIdToken();
+
+      const res = await fetch("/api/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // 'CSRF-Token': csrfToken  // HANDLED by sveltekit automatically
+        },
+        body: JSON.stringify({ idToken }),
+      });
     }
 
 </script>
