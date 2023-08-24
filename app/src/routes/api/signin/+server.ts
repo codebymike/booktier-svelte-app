bbcb9f62632616cf82e7777144bcb39faea715d1
Mike Thomas <mikethomas84@gmail.com>
2023-08-24 15:37:24 +0100

    create cookie

--- app/src/routes/api/signin/+server.ts
+++ app/src/routes/api/signin/+server.ts
@@ -11,7 +11,12 @@ export const POST: RequestHandler = async ({ request, cookies }) => {
     const decodedIdToken = await adminAuth.verifyIdToken(idToken);
 
     if (new Date().getTime() / 1000 - decodedIdToken.auth_time < 5 * 60) {
-    
+        const cookie = await adminAuth.createSessionCookie(idToken, { expiresIn });
+        const options = { maxAge: expiresIn, httpOnly: true, secure: true, path: '/' };
+
+        cookies.set('__session', cookie, options);
+
+        return json({ status: 'signedIn' });
     } else {
         throw error(401, 'Recent sign in required!');
     }
