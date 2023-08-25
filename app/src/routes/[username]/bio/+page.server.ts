544c4c40bef33c3092953403483d2186f33bc84a
Mike Thomas <mikethomas84@gmail.com>
2023-08-25 14:13:25 +0100

    try catch init

--- app/src/routes/[username]/bio/+page.server.ts
+++ app/src/routes/[username]/bio/+page.server.ts
@@ -4,4 +4,13 @@ export const load = (async ({ cookies }) => {
 
     const sessionCookie = cookies.get('__session');
 
+    try {
+
+    } catch (e) {
+        console.log(e)
+        // redirect(301, '/login');
+        throw error(401, 'Unauthorized request!')
+    }
+
+
 }) satisfies PageServerLoad;
