2eac4c38e6a3c9f8fb1a7fff46a81fbf5f4a7001
Mike Thomas <mikethomas84@gmail.com>
2023-08-29 15:17:39 +0100

    renamed component

---
+++ app/src/lib/components/UserBook.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let title = 'some cool title';
+    export let author = 'some guy';
+</script>
+
+
+<div class="stack w-full max-w-md text-center bg-base-300 flex justify-center items-center p-4 rounded-lg not-prose no-underline">
+  <span class="text-lg text-white font-bold">{title} by {author}</span>
+</div>
