63b1bd5887549928f323fe460475c8d1da53add5
Mike Thomas <mikethomas84@gmail.com>
2023-08-29 14:40:46 +0100

    reformatting

--- app/src/routes/[username]/bio/+page.server.ts
+++ app/src/routes/[username]/bio/+page.server.ts
@@ -1,5 +1,5 @@
 import type { PageServerLoad, Actions } from "./$types";
-import { adminDB } from "$lib/server/admin";
+import { adminAuth, adminDB } from "$lib/server/admin";
 import { error, fail, redirect } from "@sveltejs/kit";
 
 export const load = (async ({ locals, params }) => {
@@ -9,7 +9,7 @@ export const load = (async ({ locals, params }) => {
     if (!uid) {
       throw redirect(301, "/login");
     }
-
+  
     const userDoc = await adminDB.collection("users").doc(uid!).get();
     const { username, bio } = userDoc.data()!;
   
@@ -24,24 +24,25 @@ export const load = (async ({ locals, params }) => {
 
 export const actions = {
     default: async ({ locals, request, params }) => {
-        const uid = locals.userID;
 
-        const data = await request.formData();
-        const bio = data.get('bio');
+      const uid = locals.userID;
 
-        const userRef = adminDB.collection("users").doc(uid!);
-        const { username } = (await userRef.get()).data()!;
-
-        if (params.username !== username) {
-            throw error(401, "That username does not belong to you");
-        }
-    
-        if (bio!.length > 260) {
-            return fail(400, { problem: "Bio must be less than 260 characters" });
-        }
-        
-        await userRef.update({
-            bio
-        });
-    }
-} satisfies Actions;
+      const data = await request.formData();
+      const bio = data.get('bio');
+  
+      const userRef = adminDB.collection("users").doc(uid!);
+      const { username } = (await userRef.get()).data()!;
+  
+      if (params.username !== username) {
+        throw error(401, "That username does not belong to you");
+      }
+  
+      if (bio!.length > 260) {
+        return fail(400, { problem: "Bio must be less than 260 characters" });
+      }
+  
+      await userRef.update({
+        bio,
+      });
+    },
+  } satisfies Actions;
